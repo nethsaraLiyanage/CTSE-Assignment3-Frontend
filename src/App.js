50dd94d58923c3fb90facaca8455dba89d73e6e3
abeermahmoud62 <mahmoudabeer62@gmail.com>
2022-04-07 22:44:53 +0200

    fix-typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 // import  { useState } from 'react';
 import {Routes , Route , BrowserRouter as Router} from 'react-router-dom';
 import Announcement from './components/Announcement';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import NewsLetter from './components/Newsletter';
 import Footer from './components/Footer';
@@ -14,6 +14,7 @@ import Login from './pages/user/Login';
 import Signup from './pages/user/Signup';
 
 import Checkout from './components/Checkout';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -27,11 +28,11 @@ function App() {
           <Route path="/contact" element={<h1>contact</h1>} />
           <Route path="/register" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/cart" element={<h1>Cart</h1>} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/not-found" element={<h1>Not Found</h1>} />
           <Route path="Checkout" element={<Checkout />} />
         </Routes>
-        <NewsLetter />
+        {/* <NewsLetter /> */}
         <Footer />
       </Router>
     </div>
