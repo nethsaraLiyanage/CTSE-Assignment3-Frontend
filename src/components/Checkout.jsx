8e69ab131921db283d5836e3e4c976dcda1ddb92
AhmedBasem20 <demhamesab@gmail.com>
2022-04-07 02:32:01 +0200

    handling data

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -6,22 +6,52 @@ import reactDom from 'react-dom';
 
 const Checkout = () => {
     const checkForm = () => {
-        const [firstname, setFirst] = useState("");
-        const [lastname, setLast] = useState("");
-        const [phone, setPhone] = useState("");
-        const [email, setEmail] = useState("");
-        const [address, setAddress] = useState("");
-        const [city, setCity] = useState("");
-        const [st, setSt] = useState("");
-        const [zip, setZip] = useState("");
-        const [card, setCard] = useState("");
-        const [cvv, setCVV] = useState("");
-        const [exDate, setExDate] = useState("");
+        const [checkoutValues , setcheckoutValues] = useState({
+            firstname:"",
+            lastname:"",
+            phone:"",
+            email:"",
+            address:"",
+            city:"",
+            st:"",
+            zip:"",
+            card:"",
+            cvv:"",
+            exDate:""
+          })
+          const { firstname, lastname, phone, email, address, city, st, zip, card, cvv, exDate } 
+  = checkoutValues
 
-        function onSubmit() {
-            console.log('Name ='+ firstname + lastname);
-            console.log('Phone Number' + phone);
-        }
+  const handleChange = event => {
+    const { name, value } = event.target
+    setcheckoutValues({ ...checkoutValues, error:false, [name]: value })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    setcheckoutValues({ ...checkoutValues, error:false })
+
+          setcheckoutValues({
+            ...signUpValues, 
+            firstname: "",
+            lastname:"",
+            phone:"",
+            email:"",
+            password:"", 
+            address:"",
+            city:"",
+            st:"",
+            zip:"",
+            card:"",
+            cvv:"",
+            exDate:"",
+          })
+        
+      }
+  
+
+
+
         const state = {
             disp: "none"
         };
@@ -48,51 +78,51 @@ const Checkout = () => {
                                 <div className='col-md-6'>
                                     <div className='form-group mb-3'>
                                         <label>First Name</label>
-                                        <input type='text' name='firstname' value={firstname} onChange={(e) => setFirst(e.target.value)} required className='form-control'/>
+                                        <input type='text' name='firstname' value={firstname} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
 
                                 <div className='col-md-6'>
                                     <div className='form-group mb-3'>
                                         <label>Last Name</label>
-                                        <input type='text' name='lastname' value={lastname} onChange={(e) => setLast(e.target.value)} required className='form-control'/>
+                                        <input type='text' name='lastname' value={lastname} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
                                 <div className='col-md-6'>
                                     <div className='form-group mb-3'>
                                         <label>Phone</label>
-                                        <input type='tel' name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} required className='form-control'/>
+                                        <input type='tel' name='phone' value={phone} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
                                 <div className='col-md-6'>
                                     <div className='form-group mb-3'>
                                         <label>Email Address</label>
-                                        <input type='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required className='form-control'/>
+                                        <input type='email' name='email' value={email} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
                                 <div className='col-md-12'>
                                     <div className='form-group mb-3'>
                                         <label>Full Address</label>
-                                        <textarea rows= '3' name='address' value={address} onChange={(e) => setAddress(e.target.value)} required className='form-control'></textarea>
+                                        <textarea rows= '3' name='address' value={address} onChange={handleChange} required className='form-control'></textarea>
                                     </div>
                                 </div>
 
                                 <div className='col-md-4'>
                                     <div className='form-group mb-3'>
                                         <label>City</label>
-                                        <input type='text' name='city' value={city} onChange={(e) => setCity(e.target.value)} required className='form-control'/>
+                                        <input type='text' name='city' value={city} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
                                 <div className='col-md-4'>
                                     <div className='form-group mb-3'>
                                         <label>State</label>
-                                        <input type='text' name='state' value={st} onChange={(e) => setSt(e.target.value)} required className='form-control'/>
+                                        <input type='text' name='state' value={st} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
                                 <div className='col-md-4'>
                                     <div className='form-group mb-3'>
                                         <label>Zip Code</label>
-                                        <input type='text' name='zipcode' value={zip} onChange={(e) => setZip(e.target.value)} required className='form-control'/>
+                                        <input type='text' name='zipcode' value={zip} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
                                 <div className='col-md-6'>
@@ -104,25 +134,25 @@ const Checkout = () => {
                                 </div>
                                 
                                 {/* This div should be hidden or visible depending on the two functions above. hideDiv and showDiv */}
-                                    
+
                                 <div id='disp' style={{display: state.disp }}>
 
                                 <div className='col-md-8'>
                                     <div className='form-group mb-3'>
                                         <label>Card Number</label>
-                                        <input type='text' name='cardNumber' value={card} onChange={(e) => setCard(e.target.value)} required className='form-control'/>
+                                        <input type='text' name='cardNumber' value={card} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
                                 <div className='col-md-6'>
                                     <div className='form-group mb-3'>
                                         <label>CVV</label>
-                                        <input type='text' name='cvv' value={cvv} onChange={(e) => setCVV(e.target.value)} required className='form-control'/>
+                                        <input type='text' name='cvv' value={cvv} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
                                 <div className='col-md-6'>
                                     <div className='form-group mb-3'>
                                         <label>Expiration Date</label>
-                                        <input type='date' name='exDate' value={exDate} onChange={(e) => setExDate(e.target.value)} required className='form-control'/>
+                                        <input type='date' name='exDate' value={exDate} onChange={handleChange} required className='form-control'/>
                                     </div>
                                 </div>
 
